109bd389488f1e1dce2a11f9b906bc39d23a85eb
Bailey Benoit <Bailey.L.Benoit@gmail.com>
2024-05-07 12:55:39 -0700

    complete

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2
 
+let random = Math.floor(Math.random() * 10);
+
+let num3 = 67
+let num4 = 7
+
+let mod = num3 % num4;
+
+let max = Math.max(5, 10, 15, 20);
 // Write your code here
